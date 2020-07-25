7b75021a9ae815caf9650475a268af5690a986e0
HXgrowns <804156017@qq.com>
2020-07-25 08:41:55 +0800

    [huxiao]{modified}: modified GrammarExercise.java

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,59 @@
-import java.util.List;
+import java.util.*;
 
 public class GrammarExercise {
     public static void main(String[] args) {
         //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        Scanner scanner = new Scanner(System.in);
+        String firstWordList = scanner.nextLine();
+        String secondWordList = scanner.nextLine();
 
-        List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
+        List<String> result = findCommonWordsWithSpace(firstWordList, secondWordList);
         //按要求输出到命令行
-
+        result.forEach(System.out::println);
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        List<String> afterParseFirst = parseString(firstWordList);
+        List<String> afterParseSecond = parseString(secondWordList);
+        List<String> result = new ArrayList<>();
+
+        afterParseFirst.forEach((word) -> {
+            if (afterParseSecond.contains(word)) {
+                StringBuilder temp = new StringBuilder(word.charAt(0) + "");
+                word.chars().forEach((c) -> {
+                    temp.append(" ").append((char) c);
+                });
+                result.add(temp.substring(2).toString());
+            }
+        });
+        return result;
+    }
+
+    public static List<String> parseString(String string) {
+        String[] strings = string.toUpperCase().split(",");
+
+        HashSet<String> setStrings = new HashSet<>(Arrays.asList(strings));
+        String[] dedupStrings = setStrings.toArray(new String[0]);
+        Arrays.sort(dedupStrings);
+
+        Arrays.stream(dedupStrings).forEach((s) -> {
+            if (s.equals("")) {
+                throw new RuntimeException("input not valid");
+            }
+            s.chars().forEach((cInt) -> {
+                char c = (char) cInt;
+                if (c < 'A' || c > 'Z') {
+                    throw new RuntimeException("input not valid");
+                }
+            });
+        });
+
+        return Arrays.asList(dedupStrings);
     }
 }
+
+/*
+apple,juice,mother,people,beautiful,apple,dog
+cat,baby,smile,good,apple,beautiful,Dog,nice
+ */
